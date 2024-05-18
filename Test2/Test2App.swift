c999541939c7dd4c7a017c7d34b216959177bf9e
SandroK <sandrokupatadzee@gmail.com>
2024-05-18 12:44:30 +0400

    first commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by MacBook Pro on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
